33dd4fe65de1e89f178561e4f5a10c5c0ac0c27d
Belinda <bow@socket9.com>
2017-10-21 11:40:55 +0700

    Add avatar in navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import Lists from './components/Lists'
+import Avatar from './images/avatar.png'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <nav className="navbar">
-          <div>Test activities</div>
-          
+        <nav className="navbar block-page">
+          <div className="item-1">Test activities</div>
+          <div><img src={Avatar}/></div>
         </nav>
         <div className="container">
           <Lists></Lists>
